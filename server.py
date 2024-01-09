aa5ec56a1ad0a4ee33cfa38cb670e207919c55e1
sagar4tech <sagargupta4you@gmail.com>
2024-01-10 00:25:26 +0530

    Deploying the Program after all Unit testing and error analysis

--- server.py
+++ server.py
@@ -3,12 +3,30 @@
     localhost:5000.
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
+from flask import Flask , render_template , request 
 # Import the sentiment_analyzer function from the package created: TODO
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
 
 #Initiate the flask app : TODO
+app = Flask('Sentiment Anlayzer')
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return 'Invalid input ! Try again.'
+    else:
+        return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
+
+    
+    
+    
+    
+
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
         function. The output returned shows the label and its confidence 
@@ -18,11 +36,14 @@ def sent_analyzer():
 
 @app.route("/")
 def render_index_page():
+
+    return render_template('index.html')
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
     #TODO
 
 if __name__ == "__main__":
+    app.run(host = '0.0.0.0' , port =5001)
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
